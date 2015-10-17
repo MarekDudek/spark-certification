bc8212b90327245d2826cc88eae8c0fd2fe7e443
Marek Dudek <marek.dudek@gmail.com>
2015-10-17 22:24:51 +0200

    Parquet files processing

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ libraryDependencies ++= Seq(
 	"org.apache.spark" %% "spark-core" % "1.5.1" withSources() withJavadoc(),
 	"org.apache.spark" %% "spark-sql" % "1.5.1" withSources() withJavadoc(),
 	"org.apache.spark" %% "spark-hive" % "1.5.1" withSources() withJavadoc(),
+	"org.apache.spark" %% "spark-catalyst" % "1.5.1" withSources() withJavadoc(),
 
 	"org.scalatest"    %% "scalatest" % "2.2.4" % "test" withSources() withJavadoc()
 )
--- src/test/scala/interretis/intro/sql/PeopleWithAgesSuite.scala
+++ src/test/scala/interretis/intro/sql/PeopleWithAgesSuite.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.SQLContext
 import org.scalatest.Matchers
 import language.postfixOps
 import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.Row
 
 case class Person(name: String, age: Int)
 
@@ -21,8 +22,8 @@ class PeopleWithAgesSuite extends SeparateSparkContext with Matchers {
     val peopleLines = f.sc textFile (Resources.mainResources + "/people.txt")
     val people = peopleLines map (_ split ",") map (p => Person(p(0), p(1).trim.toInt)) toDF
 
-    people.registerTempTable("people")
-    val teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+    people registerTempTable "people"
+    val teenagers = sqlContext sql "SELECT name FROM people WHERE age >= 13 AND age <= 19"
 
     // then
     teenagers.count shouldBe 1
@@ -30,12 +31,41 @@ class PeopleWithAgesSuite extends SeparateSparkContext with Matchers {
 
   ignore should "operate on Hive" in { f =>
 
+    // given
     val hiveContext = new HiveContext(f.sc)
     import hiveContext._
     import hiveContext.implicits._
 
+    // when
     sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING)")
     sql("src/main/resources/kv1.txt' INTO TABLE src")
-    sql("FROM src SELECT key, value").collect foreach(println)
+    sql("FROM src SELECT key, value").collect foreach (println)
+  }
+
+  "Spark" should "work with Parquet files" in { f =>
+
+    // given
+    val sqlContext = new SQLContext(f.sc)
+    import sqlContext._
+    import sqlContext.implicits._
+
+    // when
+    val parquetFile = read.parquet(Resources.mainResources + "/users.parquet")
+    parquetFile registerTempTable "users"
+
+    // then
+    parquetFile.count shouldBe 2
+    parquetFile.columns shouldBe Array("name", "favorite_color", "favorite_numbers")
+
+    // when
+    val red = sql("SELECT name FROM users WHERE favorite_color = 'red'")
+    val Row(name: String) = red.first
+
+    // then
+    red.count shouldBe 1
+    name shouldBe "Ben"
+
+    // when
+    val all = sqlContext sql "SELECT * FROM users"
   }
 }
